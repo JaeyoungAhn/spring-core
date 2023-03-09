8503c87f4d455fd227165bbaf2cfdc63b53cbac8
JaeyoungAhn <quaki@naver.com>
2023-03-09 22:25:16 +0900

    no xml

---
+++ 5.Annotations/7.NoXML/1.Configuration/README.md
@@ -0,0 +1,5 @@
+1.XML configuration 대신 .java로된 config file을 사용  
+2.@Configuration과 @ComponentScan("package경로")를 config file에 명시  
+3.main 함수에 ClassPathXmlApplicationContext 대신 AnnotationConfigApplicationContext 사용
+
+
---
+++ 5.Annotations/7.NoXML/1.Configuration/SportConfig.java
@@ -0,0 +1,11 @@
+package com.spring.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.spring.springdemo")
+public class SportConfig {
+
+
+}
